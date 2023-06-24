b91dc2119b9052c96c3004f30aff0e67a51c17ef
Mahesh Gothwal <maheshgothwal1234@gmail.com>
2023-06-24 21:07:00 +0530

    iNotebook db & Express Setup

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const mongoURI = "mongodb://0.0.0.0:27017/"
+
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI, console.log("Connected to mongo successfully")
+    )
+}
+
+module.exports = connectToMongo;
