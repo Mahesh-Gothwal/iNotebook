d646bc9a80ee3f7d788bf01bb7965f9b6cff55f4
Mahesh Gothwal <maheshgothwal1234@gmail.com>
2023-07-02 22:41:22 +0530

    useLocation Hook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,26 +1,10 @@
-import { useState } from "react";
-import noteContext from "./NoteContext";
-
+import NoteContext from "./NoteContext";
 
 const NoteState = (props) =>{
-
-    const s1 = {
-        "name": "Mahesh",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1)
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name": "Mahi",
-                "class": "5b"
-            })
-        }, 1000);
-    }
     return (
-        <noteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
-        </noteContext.Provider>
+        </NoteContext.Provider>
     )
 }
 
